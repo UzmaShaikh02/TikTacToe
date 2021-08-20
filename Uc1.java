a779af36eda2ba4a91578b8ad5187e3472eb6c50
Uzma Shaikh <shaikhuzma0211@gmail.com>
2021-08-20 09:04:55 +0530

    My first game

---
+++ Uc1.java
@@ -0,0 +1,17 @@
+package TicTacToe;
+import java.util.*;
+public class Uc1 {
+	static void myMethod() {
+		char[][] board = new char[3][3];
+		
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3;j++){
+				board[i][j] = '-';
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+	}
+}
